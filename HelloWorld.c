bd7216aea443ddc1378155f83c5f63ca807d17af
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2020-02-16 13:24:06 +0600

    Some Code Changed

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	printf("This is the first init file for Git\n");
-	printf("This is the second commit\n");
-	printf("This line is inserting from VSC\n After that commiting with VSC");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("Hello World\n");
+	}
 	return 0;
 }

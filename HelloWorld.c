f65307280a8ba03b84c3e8ea9abf11692dce0ec1
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2020-02-16 13:19:05 +0600

    VSC

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,5 +4,6 @@ int main()
 {
 	printf("This is the first init file for Git\n");
 	printf("This is the second commit\n");
+	printf("This line is inserting from VSC\n After that commiting with VSC");
 	return 0;
 }

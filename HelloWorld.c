d34566031410cb9200973cf88af6592f2da621a5
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2020-02-16 13:38:12 +0600

    Dynamic Memory

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-	for (int i = 0; i < 10; i++)
+	int *arr;
+	int si;
+	scanf("%d", &si);
+	arr = (int*) malloc (si * sizeof(int)); 
+	for(int i = 0; i < si; i++)
 	{
-		printf("Hello World\n");
+		arr[i] = i;
 	}
+	for(int i = 0; i < si; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	free(arr);
+	printf("Cleaned Heap Memory\n");
 	return 0;
 }
